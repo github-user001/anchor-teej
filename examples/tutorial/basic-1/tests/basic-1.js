7ee8ea17a0f9197ce8be291fe4ca2bb7eb1eb366
Internet Person <b0x0fr0cks@pm.me>
2021-11-19 00:13:05 -0500

    Revert "Revert "almost working; something about program cant sign""
    This reverts commit 6e2a0d529fa4f9ed4696c1a27c6b3b6c343461c6.

--- examples/tutorial/basic-1/tests/basic-1.js
+++ examples/tutorial/basic-1/tests/basic-1.js
@@ -2,9 +2,43 @@ const assert = require("assert");
 const anchor = require("@project-serum/anchor");
 const { SystemProgram } = anchor.web3;
 
+/*
+
+orders
+pending
+completed
+
+create new order in pending state
+
+back office
+mark pending complete 
+
+
+
+going with just a single for now
+start small. big shits for the b[irds]
+
+[ this is created along with the transaction where the user pays ]
+
+one order 
+{
+[ selected nft ]
+token wallet address
+token mint hash 
+
+maybe maybe maybe optional shopify cart ID
+
+
+
+thoughts?
+wrapped solana?
+
+}
+*/
+
 describe("basic-1", () => {
   // Use a local provider.
-  const provider = anchor.Provider.local();
+  const provider = anchor.Provider.local("https://api.devnet.solana.com");
 
   // Configure the client to use the local cluster.
   anchor.setProvider(provider);
@@ -13,8 +47,16 @@ describe("basic-1", () => {
     // #region code-simplified
     // The program to execute.
     const program = anchor.workspace.Basic1;
-
+    const privateKey = [
+      119, 95, 179, 212, 16, 150, 55, 246, 203, 132, 41, 31, 69, 180, 137, 95,
+      117, 62, 61, 148, 140, 174, 204, 147, 193, 220, 207, 222, 91, 14, 249,
+      233, 75, 148, 53, 191, 57, 159, 93, 61, 33, 41, 100, 232, 135, 68, 94,
+      102, 17, 62, 183, 61, 243, 192, 165, 51, 54, 163, 82, 196, 107, 65, 40,
+      105,
+    ];
     // The Account to create.
+    const mike = anchor.web3.Keypair.fromSecretKey(new Uint8Array(privateKey));
+
     const myAccount = anchor.web3.Keypair.generate();
 
     // Create the new account and initialize it with the program.
@@ -22,10 +64,13 @@ describe("basic-1", () => {
     await program.rpc.initialize(new anchor.BN(1234), {
       accounts: {
         myAccount: myAccount.publicKey,
-        user: provider.wallet.publicKey,
+        user: mike.publicKey,
         systemProgram: SystemProgram.programId,
+        destination: new anchor.web3.PublicKey(
+          "A5bh7zKBchKWW9FVGNudu1J2ep95SyqK7gfhRp9ZnZpv"
+        ),
       },
-      signers: [myAccount],
+      signers: [mike, myAccount],
     });
     // #endregion code-simplified
 
@@ -36,30 +81,6 @@ describe("basic-1", () => {
     assert.ok(account.data.eq(new anchor.BN(1234)));
 
     // Store the account for the next test.
-    _myAccount = myAccount;
-  });
-
-  it("Updates a previously created account", async () => {
-    const myAccount = _myAccount;
-
-    // #region update-test
-
-    // The program to execute.
-    const program = anchor.workspace.Basic1;
-
-    // Invoke the update rpc.
-    await program.rpc.update(new anchor.BN(4321), {
-      accounts: {
-        myAccount: myAccount.publicKey,
-      },
-    });
-
-    // Fetch the newly updated account.
-    const account = await program.account.myAccount.fetch(myAccount.publicKey);
-
-    // Check it's state was mutated.
-    assert.ok(account.data.eq(new anchor.BN(4321)));
-
-    // #endregion update-test
+    // _myAccount = myAccount;
   });
 });
